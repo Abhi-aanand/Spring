76fff0f87b7b9ff9178862f77b996060182e2387
Abhi <aanand@argusoft.com>
2023-09-12 15:38:15 +0530

    Some changes

--- Spring-jpa-hibernate-crud/cruddemo JpaRepository/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
+++ Spring-jpa-hibernate-crud/cruddemo JpaRepository/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.demo.springboot.cruddemo.dao;
 
 import com.demo.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-    //no need to write code
-
+    //no need to write code here
 }
